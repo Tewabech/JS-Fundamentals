a26404e252aea3db94792e0093e25c9364410051
Tewabech Mengistu <etse30@gmail.com>
2025-07-13 11:44:42 +0300

    this is the edited version

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,18 @@
 const arg = process.argv[2];
 
+if(isNaN(arg)){
+console.log("No Argument");
+} else {
 {
+ console.log(arg);
+}
+}
+//if()
+/*{
 if ( > 0)
 console.log(num);
 
 else
 console.log("No argument");
 
-}
+}*/
--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,9 +1,16 @@
-const num=parseInt(process.argv[2]);
-{
+
+/*{
 if (num >= 0)
 console.log("My number:",num);
 
 else
 console.log("Not a number");
 
+} */
+const num=parseInt(process.argv[2]);
+if(isNaN(num)){
+console.log("Not a number");
 }
+else
+{
+return console.log("My number:",num);}
